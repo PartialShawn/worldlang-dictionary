d3c3511002c6873fae7aae3fad97c86d4d4b1c93
PartialShawn <shawn@snowbank.ca>
2026-03-23 15:22:22 +0000

    report feature i18n

--- views/reports_default_view.php
+++ views/reports_default_view.php
@@ -14,7 +14,7 @@ namespace WorldlangDict;
 
 <main id="features">
 
-<h1><?=$config->getTrans('tools button');?></h1>
+<h1><?=$config->getTrans('report list title');?></h1>
 
 
 <section>
@@ -23,16 +23,16 @@ namespace WorldlangDict;
         <p><?=$config->getTrans('report parse description');?></p>
     </div>
     <div>
-        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'new-terms', request:$request);?>"><?=$config->getTrans('report parse title');?></a></h2>
-        <p><?=$config->getTrans('report parse description');?></p>
+        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'new-terms', request:$request);?>"><?=$config->getTrans('report new entries title');?></a></h2>
+        <p><?=$config->getTrans('report new entries description');?></p>
     </div>
     <div>
-        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'updated-entries', request:$request);?>"><?=$config->getTrans('report parse title');?></a></h2>
-        <p><?=$config->getTrans('report parse description');?></p>
+        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'updated-entries', request:$request);?>"><?=$config->getTrans('report updated entries title');?></a></h2>
+        <p><?=$config->getTrans('report updated entries description');?></p>
     </div>
     <div>
-        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'removed-terms', request:$request);?>"><?=$config->getTrans('report parse title');?></a></h2>
-        <p><?=$config->getTrans('report parse description');?></p>
+        <h2><a href="<?=WorldlangDictUtils::makeUri(config:$config, controller:'report', arg:'removed-terms', request:$request);?>"><?=$config->getTrans('report removed entries title');?></a></h2>
+        <p><?=$config->getTrans('report removed entries description');?></p>
     </div>
 </section>
 
